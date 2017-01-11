fcc4d3490e2265f2ae16a018e58b29f967b209aa
Mehmet Korkmaz <mehmet@mkorkmaz.com>
2017-01-11 17:07:09 +0300

    added other abstracts

---
+++ Abstract/abstract-php-version.rb
@@ -0,0 +1,74 @@
+class AbstractPhpVersion < Formula
+  module PhpdbgDefs
+    PHPDBG_SRC_TARBAL = 'https://github.com/krakjoe/phpdbg/archive/v0.3.2.tar.gz'
+    PHPDBG_CHECKSUM   = {
+                        :sha256 => 'feab6e29ef9a490aa53332fe014e8026d89d970acc5105f37330b2f31e711bbd',
+                      }
+  end
+
+  module Php53Defs
+    PHP_SRC_TARBALL = 'https://php.net/get/php-5.3.29.tar.bz2/from/this/mirror'
+    PHP_GITHUB_URL  = 'https://github.com/php/php-src.git'
+    PHP_VERSION     = '5.3.29'
+    PHP_BRANCH      = 'PHP-5.3'
+
+    PHP_CHECKSUM    = {
+                        :sha256 => 'c4e1cf6972b2a9c7f2777a18497d83bf713cdbecabb65d3ff62ba441aebb0091',
+                      }
+  end
+
+  module Php54Defs
+    PHP_SRC_TARBALL = 'https://php.net/get/php-5.4.45.tar.bz2/from/this/mirror'
+    PHP_GITHUB_URL  = 'https://github.com/php/php-src.git'
+    PHP_VERSION     = '5.4.45'
+    PHP_BRANCH      = 'PHP-5.4'
+
+    PHP_CHECKSUM    = {
+                        :sha256 => '4e0d28b1554c95cfaea6fa2b64aac85433f158ce72bb571bcd5574f98f4c6582',
+                      }
+  end
+
+  module Php55Defs
+    PHP_SRC_TARBALL = 'https://php.net/get/php-5.5.38.tar.bz2/from/this/mirror'
+    PHP_GITHUB_URL  = 'https://github.com/php/php-src.git'
+    PHP_VERSION     = '5.5.38'
+    PHP_BRANCH      = 'PHP-5.5'
+
+    PHP_CHECKSUM    = {
+                        :sha256 => '473c81ebb2e48ca468caee031762266651843d7227c18a824add9b07b9393e38',
+                      }
+  end
+
+  module Php56Defs
+    PHP_SRC_TARBALL = 'https://php.net/get/php-5.6.29.tar.bz2/from/this/mirror'
+    PHP_GITHUB_URL  = 'https://github.com/php/php-src.git'
+    PHP_VERSION     = '5.6.29'
+    PHP_BRANCH      = 'PHP-5.6'
+
+    PHP_CHECKSUM    = {
+                        :sha256 => '499b844c8aa7be064c111692e51a093ba94e54d2d9abb01e70ea76183a1825bb',
+                      }
+  end
+
+  module Php70Defs
+    PHP_SRC_TARBALL = 'https://php.net/get/php-7.0.14.tar.bz2/from/this/mirror'
+    PHP_GITHUB_URL  = 'https://github.com/php/php-src.git'
+    PHP_VERSION     = '7.0.14'
+    PHP_BRANCH      = 'PHP-7.0'
+
+    PHP_CHECKSUM    = {
+                        :sha256 => 'fbc4369a0d42b55fd1ce75eb4f3d17b012da754a67567d8e3288fbfbb7490534',
+                      }
+  end
+
+  module Php71Defs
+    PHP_SRC_TARBALL = 'https://php.net/get/php-7.1.0.tar.bz2/from/this/mirror'
+    PHP_GITHUB_URL  = 'https://github.com/php/php-src.git'
+    PHP_VERSION     = '7.1.0'
+    PHP_BRANCH      = 'PHP-7.1'
+
+    PHP_CHECKSUM    = {
+                        :sha256 => '68bcfd7deed5b3474d81dec9f74d122058327e2bed0ac25bbc9ec70995228e61',
+                      }
+  end
+end
